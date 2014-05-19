debf6ea7774109d747d8f13b779d4a2bc691cd30
Matti Paksula <matti.paksula@iki.fi>
2014-05-19 21:54:40 +0300

    added navigationbar with reload and restart

--- NativeBridgeBenchmark/BenchmarkViewController.m
+++ NativeBridgeBenchmark/BenchmarkViewController.m
@@ -9,24 +9,55 @@
 #import "BenchmarkViewController.h"
 
 @interface BenchmarkViewController ()
-
+@property(nonatomic, retain) UIWebView *webView;
 @end
 
 @implementation BenchmarkViewController
 
 - (void)loadView
 {
-    UIWebView *webView = [[UIWebView alloc] init];
+    [self setWebView:[UIWebView new]];
+    [self setView: self.webView ];
+
+    [self restart];
+
+    [self addNavigationBar];
     
-    
+}
+
+-(void)addNavigationBar
+{
+    UINavigationBar *navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+
+
+    navBar.backgroundColor = [UIColor yellowColor];
+
+    UINavigationItem *navItem = [[UINavigationItem alloc] init];
+    navItem.title = @"Benchmark";
+
+    UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithTitle:@"Reload" style:UIBarButtonItemStylePlain target:self action:@selector(reload)];
+    navItem.leftBarButtonItem = leftButton;
+
+    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:@"Restart" style:UIBarButtonItemStylePlain target:self action:@selector(restart)];
+    navItem.rightBarButtonItem = rightButton;
+
+
+    navBar.items = @[ navItem ];
+
+    [self.view insertSubview:navBar aboveSubview: self.view];
+}
+
+-(void)reload
+{
+    [[ self webView ] stringByEvaluatingJavaScriptFromString:@"window.location.reload();"];
+}
+
+-(void)restart {
     NSString *localHTMLPath = [NSBundle.mainBundle pathForResource:@"index" ofType:@"html"];
     NSURL *localHTMLURL = [NSURL fileURLWithPath:localHTMLPath];
     NSURLRequest *request = [NSURLRequest requestWithURL: localHTMLURL];
     
-    [webView loadRequest: request ];
-    
-    [self setView: webView];
-    
+    [self.webView loadRequest: request ];
 }
 
 - (void)viewDidLoad
