0eebc76fd20fdd78289de8b89b00d8544a73b48c
Matti Paksula <matti.paksula@iki.fi>
2014-05-31 22:50:46 +0300

    changed scrolling to be sideways

--- NativeBridgeBenchmark/BenchmarkViewController.m
+++ NativeBridgeBenchmark/BenchmarkViewController.m
@@ -235,6 +235,11 @@ BenchmarkViewController* gController;
     [self.webView.scrollView setScrollIndicatorInsets:UIEdgeInsetsMake(44, 0, 0, 0)];
     [self.webView.scrollView scrollRectToVisible:CGRectMake(0, 0, 1, 1) animated:NO];
 
+    self.webView.paginationMode = UIWebPaginationModeLeftToRight;
+    self.webView.paginationBreakingMode = UIWebPaginationBreakingModePage;
+    self.webView.gapBetweenPages = 10;
+
+    self.webView.scrollView.bounces = false;
 
     [ self.webView setDelegate:self];
 
