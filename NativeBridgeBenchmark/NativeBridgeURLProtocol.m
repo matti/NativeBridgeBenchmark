8c6a85925cd4890ac8c2808006542f04f51e8bd0
Matti Paksula <matti.paksula@iki.fi>
2014-09-24 18:45:29 +0300

    urlprotocol unfucked

--- NativeBridgeBenchmark/NativeBridgeURLProtocol.m
+++ NativeBridgeBenchmark/NativeBridgeURLProtocol.m
@@ -8,14 +8,21 @@
 
 #import "NativeBridgeURLProtocol.h"
 
+#import <MobileCoreServices/MobileCoreServices.h>
+
 @implementation NativeBridgeURLProtocol
 
 
 +(BOOL) canInitWithRequest:(NSURLRequest *)request {
-    if ([ [[request URL] scheme ] isEqualToString:@"nativebridge" ]) {
+
+    if ([ [[request URL] host ] isEqualToString:@"nativebridge" ]) {
+
         NSLog(@"URLPROTOCOL captured");
 
-        NSLog([[request URL] path]);
+        
+        // TODO: this just might be it:
+//            [self.client URLProtocol:self didFailWithError:[NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorZeroByteResource userInfo:nil]];
+        
         return YES;
     };
     
@@ -24,8 +31,50 @@
 //        return YES;
 //    }
 //    
-    
+
+    // Do not capture, fall back to webview default behaviour
     return NO;
 }
+
++ (NSURLRequest*) canonicalRequestForRequest:(NSURLRequest*)request {
+    //    NSMutableURLRequest *copy = [[request mutableCopy] autorelease];
+    //    copy.URL = [NSURL URLWithString:[[[request URL] absoluteString] stringByReplacingOccurrencesOfString:@"http" withString:[NSString stringWithFormat:@"http+%f", [[NSDate date] timeIntervalSince1970]]]];
+    return request;
+}
+
+-(void) startLoading {
+    NSString *requestPath = [[[self request] URL] path];
+    NSString *requestMethod = [[self request] HTTPMethod];
+
+    NSData *body = [@"{}" dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSHTTPURLResponse *response = [NSHTTPURLResponse alloc];
+    
+    [response initWithURL:[[self request] URL]
+                statusCode:200
+               HTTPVersion:@"HTTP/1.1"
+              headerFields: @{
+                              @"Connection": @"keep-alive",
+                              @"Cache-Control": @"public, max-age=0",
+                              @"Content-Type": @"application/javascript",
+                              @"Access-Control-Allow-Origin": @"*"
+                              }];
+    
+    [self respondWithResponse:response Body:body];
+}
+
+-(void) stopLoading {
+    
+}
+
+-(void) respondWithResponse:(NSHTTPURLResponse*)response Body:(NSData*)body {
+    [[self client] URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+    
+    [[self client] URLProtocol:self didLoadData:body];
+    
+    [[self client] URLProtocolDidFinishLoading:self];
+}
+
+
 
 @end
