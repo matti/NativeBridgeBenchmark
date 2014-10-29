3ef6e15f5feb425d5ba3e78efef04e68ecb25cbd
Matti Paksula <matti.paksula@iki.fi>
2014-10-29 22:32:49 +0200

    moved to companion

--- Web/WebSocketTest2.js
+++ Web/WebSocketTest2.js
@@ -1,21 +1,3 @@
 function WebSocketTest2(url)
 {
-	if ("WebSocket" in window)
-	{
-		var ws = new WebSocket("%%WEBSOCKET_URL%%");
-		ws.onopen = function()
-		{
-			// Web Socket is connected
-//			alert("websocket is open");
-
-			// You can send data now
-			ws.send(url);
-		};
-		ws.onmessage = function(evt) { alert("received: " + evt.data); };
-		ws.onclose = function() { alert("websocket is closed"); };
-	}
-	else
-	{
-		alert("Browser doesn't support WebSocket!");
-	}
 }
