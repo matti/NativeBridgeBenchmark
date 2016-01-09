d27cee0ee2277855da49ed219daf0b33d76ea122
Matti Paksula <matti.paksula@iki.fi>
2016-01-09 20:37:46 +0200

    xhrpost xhrget

--- NativeBridgeBenchmark/MyHTTPConnection.m
+++ NativeBridgeBenchmark/MyHTTPConnection.m
@@ -20,10 +20,42 @@ static const int httpLogLevel = HTTP_LOG_LEVEL_WARN; // | HTTP_LOG_FLAG_TRACE;
 
 @implementation MyHTTPConnection
 
+-(BOOL) supportsMethod:(NSString *)method atPath:(NSString *)path {
+    return YES;
+}
+
+- (BOOL)expectsRequestBodyFromMethod:(NSString *)method atPath:(NSString *)path
+{
+    HTTPLogTrace();
+    
+    // Inform HTTP server that we expect a body to accompany a POST request
+    
+    if([method isEqualToString:@"POST"])
+        return YES;
+    
+    return [super expectsRequestBodyFromMethod:method atPath:path];
+}
+
+- (void)processBodyData:(NSData *)postDataChunk
+{
+    HTTPLogTrace();
+    
+    // Remember: In order to support LARGE POST uploads, the data is read in chunks.
+    // This prevents a 50 MB upload from being stored in RAM.
+    // The size of the chunks are limited by the POST_CHUNKSIZE definition.
+    // Therefore, this method may be called multiple times for the same POST request.
+    
+    BOOL result = [request appendData:postDataChunk];
+    if (!result)
+    {
+        HTTPLogError(@"%@[%p]: %@ - Couldn't append bytes!", THIS_FILE, self, THIS_METHOD);
+    }
+}
+
 - (NSObject<HTTPResponse> *)httpResponseForMethod:(NSString *)method URI:(NSString *)path
 {
     
-    // xhrlocal.async and xhrlocal.sync + xhrlocal.pongweb
+    // xhrget.* and xhrpost.*
     if ([path hasPrefix:@"/nativebridge://"]) {
     
         NativeEvent *nativeEvent = nil;
